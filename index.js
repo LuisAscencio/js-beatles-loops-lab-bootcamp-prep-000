4b983471795fef8ded9a947465924cdaa6386411
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-20 17:52:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function theBeatlesPlay(musicians, instruments) {
 }  var array = []
-  for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i])
-
-  return array{ }
+  for (let i = 0; i < musicians.length; i++) 
+  {
+    var string = `${musicians[i]} plays ${instruments[i]}`
+    array.push(string)
+  }
+  return array{
 }
