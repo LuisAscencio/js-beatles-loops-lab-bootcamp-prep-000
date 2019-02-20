fc49008fafc3c009551f3b4fc5c2588f1d1a9b4f
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-20 18:00:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts (facts)
+ var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array
+}
