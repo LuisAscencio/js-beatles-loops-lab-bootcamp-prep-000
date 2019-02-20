07a972b9248f6b750348d8ea2fcfd4d7747d2f81
Luis Ascencio <luismiguelascencio@gmail.com>
2019-02-20 18:19:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function iLoveTheBeatles (Number){
   do {
     array.push('I love the Beatles!')
     i++
-  } while (i <= number && number < 15)
+  } 
+  
+  while (i <= number && number < 15)
   return array
 
 }
